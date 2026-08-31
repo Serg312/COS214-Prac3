3df2e06b794778b733c0368e049f7d8fcaa1341a
That_Guy312 <ferreirasergio624@gmail.com>
2026-08-31 14:49:33 +0200

    Add EventUnit implementation

--- src/EventUnit.cpp
+++ src/EventUnit.cpp
@@ -1,9 +1,9 @@
 #include "EventUnit.h"
 
-EventUnit::EventUnit(const std::string& unitName, int unitCapacity)
-    : EventComponent(unitName), capacity(unitCapacity), isOpen(false) {}
+EventUnit::EventUnit(const std::string& unitName, int unitCapacity) : EventComponent(unitName), capacity(unitCapacity), isOpen(false) {}
 
-int EventUnit::getCapacity() const {
+int EventUnit::getCapacity() const 
+{
     return capacity;
 }
 
