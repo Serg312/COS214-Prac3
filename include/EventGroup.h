5cb32bc1beeb1809643fc8fe2588443bdcd66e82
Rafael de Gouveia <rafaeldegouveia9@gmail.com>
2026-08-31 17:10:01 +0200

    Added the doxyfile audit for EventGroup.h

--- include/EventGroup.h
+++ include/EventGroup.h
@@ -4,18 +4,33 @@
 #include "EventComponent.h"
 #include <vector>
 
+/**
+ * @brief Represents a group of event components within the event system.
+ */
 class EventGroup : public EventComponent 
 {
     private:
+        /// @brief The list of child components within the event group.
         std::vector<EventComponent*> children;
     public:
+        /**
+         * @brief Constructs a new EventGroup instance.
+         * @param groupName The name of the event group, used for identification and reporting.
+         */
         EventGroup(const std::string& groupName);
+        /// @brief Adds a component to the event group.
         void add(EventComponent* component);
+        /// @brief Removes a component from the event group.
         bool remove(EventComponent* component);
+        /// @brief Opens all components within the event group.
         void open() override;
+        /// @brief Closes all components within the event group.
         void close() override;
+        /// @brief Reports the current status of the event group.
         void reportStatus() const override;
+        /// @brief Gets the capacity of the event group.
         int getCapacity() const override;
+        /// @brief Destructor for the EventGroup class.
         virtual ~EventGroup();
 };
 
